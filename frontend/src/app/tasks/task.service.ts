4a8d082b7a6843d9343da6717b4ee0c93f129252
Masith Jayarathna <masith.jayarathna@osos.om>
2025-05-06 18:00:26 +0530

    feat: add task service to handle task management

---
+++ frontend/src/app/tasks/task.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from './task.model'; 
+import { AuthService } from '../auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TaskService {
+  private apiUrl = 'http://localhost:8080/api/tasks';
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  getTasks(): Observable<Task[]> {
+    console.log('Fetching tasks from API');
+    const headers = this.authService.getTokenHeader();
+    console.log('Headers:', headers);
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+  deleteTask(id: number): Observable<string> {
+    return this.http.delete(`${this.apiUrl}/${id}`, { responseType: 'text' });
+  }
+  
+
+  updateTask(id: number, task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${id}`, task);
+  }
+}
